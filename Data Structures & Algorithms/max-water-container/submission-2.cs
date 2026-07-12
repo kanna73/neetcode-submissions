6dafa4479baf8c33e033aec4faceb8bd010dda48
Kannappan <127297283+kanna73@users.noreply.github.com>
2026-07-12 22:11:35 +0530

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int MaxArea(int[] heights) {
+        int n = heights.Length;
+        int j = n-1;
+        int i =0;
+        int result = 0;
+        while(i<j){ // If both pointers are supposed to move dynamically based on conditions, use a while loop instead of a for loop.
+            int x1 = i+1;
+            int x2 = j+1;
+            int mut = x2-x1;
+            if(heights[i]>heights[j]){
+                result = result < mut*heights[j] ? mut*heights[j] : result;
+                j--;
+            }
+            else if (heights[j]>heights[i]){
+                result = result < mut*heights[i] ? mut*heights[i] : result;
+                i++;
+            }
+            else{
+                result = result < mut*heights[i] ? mut*heights[i] : result;
+                i++;
+            }
+        }
+        return result;
+    }
+}
