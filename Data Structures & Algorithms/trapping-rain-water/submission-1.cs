e1ef26a325f93154d8397f1954e23979eddb7eea
Kannappan <127297283+kanna73@users.noreply.github.com>
2026-07-18 20:46:11 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int Trap(int[] height) {
+       int count = height.Length;
+       int i = 0;
+       int j = 1;
+       int result =0;
+       int temp = 0;
+
+       while(j<count){
+
+        if(height[i]>height[j]){
+            temp += height[i]-height[j];
+            j= j+1;
+            if(j==count-1){
+                i= i+1;
+                j = i+1;
+                continue;
+            }
+            continue;
+        }
+        if(height[j]>=height[i]){
+            result += temp;
+            temp=0;
+            i = j;
+            j = j+1;
+            continue;
+        }
+       }
+       return result;
+    }
+}
